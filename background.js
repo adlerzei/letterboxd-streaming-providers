32da0dfb79278ebf312687fd9564937efea44c58
Christian Zei <christianzei@gmx.net>
2019-02-19 12:38:32 +0100

    background.js: add methods for getting the streaming flatrates for all movies on web page getFilmsFromLetterboxd.js: a content script, which returns all movies from a web page

--- background.js
+++ background.js
@@ -3,8 +3,12 @@
 //for compatibility reasons
 var browser = chrome;
 
+var provider_id = 8; //netflix: 8, amazon prime: 9
+
+var availableMovies = [];
+
 /**
- * Returns a
+ * Checks if a movie is available and adds it to availableMovies
  *
  * @param {string} toFind - The name of the movie, for which the database should be queried.
  * @returns {Promise<void>} - An empty Promise if the API calls worked correctly, else the Promise contains the respective errors.
@@ -12,7 +16,8 @@ var browser = chrome;
  */
 async function isIncluded(toFind) {
   //e.g. toFind = 'Suck me Shakespeer'
-  var eng_title = toFind;
+  var eng_title = toFind.title;
+  var movie_release_year = toFind.year;
 
   var param = eng_title.replace(' ', '%20');
 
@@ -27,11 +32,11 @@ async function isIncluded(toFind) {
   xhttp.send();
 
   xhttp.onreadystatechange = function () {
-    if(xhttp.readyState === 4 && xhttp.status === 200) {
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
       title_rsp = JSON.parse(xhttp.response);
 
-      for(let item in title_rsp.results) {
-        if(title_rsp.results[item].title.toLowerCase() === eng_title.toLowerCase()) {
+      for (let item in title_rsp.results) {
+        if (title_rsp.results[item].title.toLowerCase() === eng_title.toLowerCase()) {
           original_title = title_rsp.results[item].original_title
         }
       }
@@ -45,19 +50,47 @@ async function isIncluded(toFind) {
       xhttp.send();
 
       xhttp.onreadystatechange = function () {
-        if(xhttp.readyState === 4 && xhttp.status === 200) {
+        if (xhttp.readyState === 4 && xhttp.status === 200) {
           rsp = JSON.parse(xhttp.response);
 
-          for(let item in rsp.items) {
-            if(rsp.items[item].original_title.toLowerCase() === original_title.toLowerCase()) {
+          for (let item in rsp.items) {
+            if (rsp.items[item].original_title.toLowerCase() === original_title.toLowerCase()) {
               for (let offer in rsp.items[item].offers) {
-                console.log(rsp.items[item].offers[offer]);
+                if (rsp.items[item].offers[offer].monetization_type === 'flatrate' && rsp.items[item].offers[offer].provider_id === provider_id) {
+                  availableMovies.push(toFind);
+                }
               }
             }
           }
         }
       }
     }
+  }
+}
+
+function getFilmsFromLetterboxd() {
+  browser.tabs.executeScript({
+    file: "scripts/getFilmsFromLetterboxd.js",
+    allFrames: false
+  })
+}
+
+browser.runtime.onMessage.addListener(handleMessage);
+
+function handleMessage(request, sender, sendResponse) {
+  if (request.hasOwnProperty('message_type')) {
+    if(request.message_type === 'movie-titles') {
+      checkMovieAvailability(request.message_content);
+    }
+  }
+}
+
+function checkMovieAvailability(movies) {
+  for(let movie in movies) {
+    var inc = isIncluded({
+      title: movie,
+      year: movies[movie]
+    })
   }
 }
 
