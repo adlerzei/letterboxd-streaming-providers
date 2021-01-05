3d761ec5dbaacaba0ecabcadefb102145da6d523
Christian Zei <christian.zei@stud.tu-darmstadt.de>
2021-01-05 10:08:24 +0100

    Increase copyright year

--- extension/scripts/getFilmsFromLetterboxd.js
+++ extension/scripts/getFilmsFromLetterboxd.js
@@ -1,6 +1,6 @@
 /*!
  *
- *     Copyright (c) 2019 Christian Zei
+ *     Copyright (c) 2021 Christian Zei
  *
  *     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
