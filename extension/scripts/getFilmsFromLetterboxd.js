705f93a35c69f0cf57049af41b56df0294c3b2cb
Christian Zei <christian.zei@stud.tu-darmstadt.de>
2021-01-06 18:37:06 +0100

    Remove unnecessary distinction between '/watchlist' and other subpages

--- extension/scripts/getFilmsFromLetterboxd.js
+++ extension/scripts/getFilmsFromLetterboxd.js
@@ -17,27 +17,27 @@ if (typeof browser === 'undefined') {
 	var browser = chrome;
 }
 
-var filmposters = document.body.getElementsByClassName('film-poster');
+var filmposters = document.body.getElementsByClassName('poster-container');
 
 var movies = {};
 for (let poster = 0; poster < filmposters['length']; poster++) {
-	if (filmposters[poster].attributes.hasOwnProperty('data-film-name')) {
-		let filmName = filmposters[poster].attributes['data-film-name'].value;
+	if (filmposters[poster].children[0].attributes.hasOwnProperty('data-film-name')) {
+		let filmName = filmposters[poster].children[0].attributes['data-film-name'].value;
 
 		if (movies.hasOwnProperty(filmName)) {
 			if (movies[filmName].year === -1) {
-				movies[filmName].year = filmposters[poster].attributes['data-film-release-year'].value;
+				movies[filmName].year = filmposters[poster].children[0].attributes['data-film-release-year'].value;
 			}
 
 			movies[filmName].id.push(poster);
 		} else {
 			movies[filmName] = {
-				year: filmposters[poster].attributes['data-film-release-year'].value,
+				year: filmposters[poster].children[0].attributes['data-film-release-year'].value,
 				id: [poster]
 			};
 		}
 	} else {
-		let filmName = filmposters[poster].children[0].alt;
+		let filmName = filmposters[poster].children[0].children[0].alt;
 
 		if (movies.hasOwnProperty(filmName)) {
 			movies[filmName].id.push(poster);
